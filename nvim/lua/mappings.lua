c04909061f46fb431e2b5665b5b2b7519502c600
Mike Doan <manhdoanmainmail@gmail.com>
2024-04-01 14:09:28 -0400

    better keybindings on nvim

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -6,6 +6,9 @@ local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
+map("n", "J", "10j", { desc = "Reasonably go down" })
+map("n", "U", "10k", { desc = "Reasonably go up " })
+
 
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
