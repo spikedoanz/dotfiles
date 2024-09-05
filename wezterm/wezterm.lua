c5f266f6857b6ba561ed76efaa6c694c565057c0
Mike Doan <spikedoanz@gmail.com>
2024-09-05 15:00:09 -0400

    gca

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -29,69 +29,8 @@ c.window_padding = {
   top = 0,
   bottom = 0,
 }
-
-c.colors = {
-  foreground = "#cdd6f4",
-  background = "#1e1e2e",
-  cursor_bg = "#f5e0dc",
-  cursor_fg = "#1e1e2e",
-  cursor_border = "#f5e0dc",
-  selection_fg = "#1e1e2e",
-  selection_bg = "#f5e0dc",
-  scrollbar_thumb = "#45475a",
-  split = "#45475a",
-  
-  ansi = {
-    "#45475a",
-    "#f38ba8",
-    "#a6e3a1",
-    "#f9e2af",
-    "#89b4fa",
-    "#f5c2e7",
-    "#94e2d5",
-    "#bac2de",
-  },
-  brights = {
-    "#585b70",
-    "#f38ba8",
-    "#a6e3a1",
-    "#f9e2af",
-    "#89b4fa",
-    "#f5c2e7",
-    "#94e2d5",
-    "#a6adc8",
-  },
-  indexed = {
-    [16] = "#fab387",
-    [17] = "#f5e0dc",
-  },
-  -- Tab bar colors
-  tab_bar = {
-    background = "#1e1e2e",
-    active_tab = {
-      bg_color = "#1e1e2e",
-      fg_color = "#cdd6f4",
-    },
-    inactive_tab = {
-      bg_color = "#1e1e2e",
-      fg_color = "#6c7086",
-    },
-    inactive_tab_hover = {
-      bg_color = "#1e1e2e",
-      fg_color = "#a6adc8",
-    },
-    new_tab = {
-      bg_color = "#1e1e2e",
-      fg_color = "#6c7086",
-    },
-    new_tab_hover = {
-      bg_color = "#1e1e2e",
-      fg_color = "#a6adc8",
-    },
-  }
-}
+c.color_scheme = 'Catppuccin Mocha'
 c.use_fancy_tab_bar = false
 c.hide_tab_bar_if_only_one_tab = true
-c.window_background_opacity = 1.0
 
 return c
