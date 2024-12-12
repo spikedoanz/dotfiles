e0de51019662acf7c26a95d43d870d79cdac7c56
spike <spikedoanz@gmail.com>
2024-12-12 15:38:46 -0500

    wp

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -9,8 +9,9 @@ end
 
 local act = wezterm.action
 
--- Keys
+-- Keybinding
 c.keys = {
+  -- window bindings
   { key="-", mods="ALT", action=act.SplitVertical({ domain = "CurrentPaneDomain" }), },
   { key="=", mods="ALT", action=act.SplitHorizontal({ domain = "CurrentPaneDomain" }), },
   { key="Q", mods="ALT", action=act.CloseCurrentPane({ confirm = true }) },
@@ -18,19 +19,29 @@ c.keys = {
   { key="L", mods="ALT", action=act.ActivateTabRelative(1) },
   { key="h", mods="ALT", action=act.ActivatePaneDirection("Prev")},
   { key="l", mods="ALT", action=act.ActivatePaneDirection("Next")},
+  -- vim bindings
+  { key = 'b', mods = 'ALT', action = act.SendString '\x1bb' },
+  { key = 'w', mods = 'ALT', action = act.SendString '\x1bf' },
+  { key = '0', mods = 'ALT', action = act.SendString '\x01' },
+  { key = '4', mods = 'ALT', action = act.SendString '\x05' }, -- alias for $, but is the same thing
+  { key = '$', mods = 'ALT', action = act.SendString '\x05' },
+  { key = 'D', mods = 'ALT', action = act.SendString '\x0b' },
 }
 
 -- Appearance
-c.font = wezterm.font('JetBrainsMono Nerd Font')
-c.font_size = 12
+c.font = wezterm.font('JetBrainsMono Nerd Font Mono')
+c.font_size = 24
 c.window_padding = {
   left = 0,
   right = 0,
   top = 0,
   bottom = 0,
 }
+c.enable_kitty_graphics = false
+c.enable_sixel = true
 c.color_scheme = 'Catppuccin Mocha'
 c.use_fancy_tab_bar = false
 c.hide_tab_bar_if_only_one_tab = true
+c.max_fps = 144
 
 return c
